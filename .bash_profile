09a702f30b730f90701816ce66e948e4352371b4
Marko Tibold <marko@tibold.nl>
2012-12-19 22:01:45 +0100

    Merged with my own settings. Still great stuff.

--- .bash_profile
+++ .bash_profile
@@ -1,8 +1,3 @@
-# Load .bashrc if it exists
-if [ -f ~/.bashrc ]; then
-   source ~/.bashrc
-fi
-
 # brew bash completion
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
     . $(brew --prefix)/etc/bash_completion
@@ -17,3 +12,12 @@ source ~/.dotfiles/.bash_prompt
 # Prefer US English and use UTF-8
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US"
+
+# virtualenvwrapper related
+source /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME="~/mac_envs"
+export PATH="/usr/local/bin:/usr/local/mysql/bin:$PATH"
+
+# Hack for mysql-python, TODO really solve this
+export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,6 @@ echo "symlink .bash_profile"
 rm -f ~/.bash_profile
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
-echo "symlink .screenrc"
-rm -f ~/.screenrc
-ln -s ~/.dotfiles/.screenrc ~/.screenrc
-
 echo "symlink .gitconfig"
 rm -f ~/.gitconfig
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -15,16 +11,6 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 echo "symlink .gitignore"
 rm -f ~/.gitignore
 ln -s ~/.dotfiles/.gitignore ~/.gitignore
-
-echo "symlink Sublime Text preferences and keymap"
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/.dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
-
-echo "symlink Sublime Text command line"
-rm -f /usr/local/bin/subl
-ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
 echo "setup osx settings"
 ./.osx
